70102d65582fa46715bc6d6e6434e1bdb5004a14
danielt17 <dtyytlman@gmail.com>
2025-07-04 18:39:57 +0300

    minor pep8 fix

--- pyEliashMEM/main.py
+++ pyEliashMEM/main.py
@@ -3,6 +3,7 @@ import os
 from pyEliashMEM.utils.read_inputs import read_inputs, read_parameters_in_file
 from pyEliashMEM.utils.read_dispersion_in_file import read_and_shift_dispersion_data
 
+
 def main():
     inputs = read_inputs()
     filepath_ini = os.path.join(inputs["input_parameters_folder"], inputs["input_parameters_file"])
